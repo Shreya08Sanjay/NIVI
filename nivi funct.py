b600cd11e88c41fc5b844facec35f56b8adcd031
Shreya <shreya081105@gmail.com>
2023-11-27 21:37:41 +0400

    YO GENG GENG

---
+++ nivi funct.py
@@ -0,0 +1,8 @@
+def nivi():
+    name=input('Enter name:')
+    if name in ['nivi','NIVI','Nivi']:
+        print('yea ik that bitch she sucks')
+    else:
+        print('whoever that is, is swager than nivi for sure')
+
+nivi()
